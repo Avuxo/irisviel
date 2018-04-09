5b65ae1b0803982aa55d0e9c301d446552d38249
Ben <ben@july7.pw>
2018-04-09 11:22:44 -0400

    remove use of c++ comments. Used them out of habit.

--- src/main.c
+++ src/main.c
@@ -32,16 +32,16 @@ int main(int argc, char **argv){
     iris_registerRoute(env, "/", &route_index);
     iris_registerRoute(env, "/about", &route_about);
 
-    // no args passed.
-    // this would be like if i did /:whatever wherever whatever's value is an arg
-    // arguments are URL-encoded and separated by spaces.
-    // this would be called by the webserver, this is simply an example.
+    /* no args passed.
+       this would be like if i did /:whatever wherever whatever's value is an arg
+       arguments are URL-encoded and separated by spaces.
+       this would be called by the webserver, this is simply an example.*/
     char *page = iris_getRoute(env, "/", "");
-    free(page); // free the page because it needs to be malloc()'d for sprintf
+    free(page); /* free the page because it needs to be malloc()'d for sprintf
                 // to write into the buffer
-                // this is only called after the response is sent to the client
+                // this is only called after the response is sent to the client.*/
 
-    // about page
+    /*about page*/
     char *aboutPage = iris_getRoute(env, "/about", "");
     free(aboutPage); 
 
